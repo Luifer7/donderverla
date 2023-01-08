eb620d26390931a9090f244401e181b4aff1c7d7
luifer7 <corderoluisfernando7@gmail.com>
2023-01-07 20:49:41 -0500

    07/01-c

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,22 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   
-  <div class="text-center w-100 banner mb-3 row m-auto">
+  <div class="text-center w-100 banner mb-3 row m-auto ">
 
       <div class="col-12 col-sm-4  d-flex align-items-center justify-content-center" >
         <i class="bi bi-house-heart text-white"> Mi espacio</i>
       </div>
       
       <div class="col-12 col-sm-4  d-flex align-items-center justify-content-center donde" >
+
         <router-link to="/" class="text-decoration-none v" >
-          <h2 class="m-0  text-center" ><i>¿DÓNDE VERLA?</i></h2>
+          <h3 class="m-0  text-center" ><i>¿DÓNDE VERLA?</i></h3>
       </router-link>
       
       </div>
     
-      <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center" >
+     
+       <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center" >
         <router-link to="/sobrenosotros" class="text-white fw-bold text-decoration-none" >
           <i class="bi bi-info-circle">
             Sobre nosotros
@@ -51,13 +53,14 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
 
-
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Spray+Paint&display=swap');
 
 .banner {
-  background-image: url(../src/assets/img/banner.jpg);
+  background-image: url(../src/assets/img/ggghh.jpg);
   background-size: cover;
+  background-position: center;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  height: 150px;
+  height: 200px;
 }
 
 .vistas-enter-from {
@@ -79,10 +82,12 @@ import { RouterLink, RouterView } from 'vue-router'
 
 .donde .v{
   font-weight: 600;
-  background-image: linear-gradient(to left, #553c9a, #b393d3);
+  background-image: linear-gradient(109.6deg, rgb(255, 252, 252) 11.2%, rgb(11, 132, 145) 91.1%);
   color: transparent;
   background-clip: text;
   -webkit-background-clip: text;
+  font-family: 'Rubik Spray Paint', cursive;
+  text-shadow: rgba(255, 255, 255, 0.849) 3px -24px 2px;
 }
 
 
